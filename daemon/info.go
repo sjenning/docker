9cad141edae4cde702523aeabe434faa3f6c459f
Dan Walsh <dwalsh@redhat.com>
2016-01-05 08:11:05 -0500

    Return rpm version of docker package in docker version
    Signed-off-by: Dan Walsh <dwalsh@redhat.com>
    Signed-off-by: Antonio Murdaca <runcom@redhat.com>

--- daemon/info.go
+++ daemon/info.go
@@ -2,6 +2,7 @@ package daemon
 
 import (
 	"os"
+	"os/exec"
 	"runtime"
 	"strings"
 	"sync/atomic"
@@ -14,6 +15,7 @@ import (
 	"github.com/docker/docker/pkg/parsers/kernel"
 	"github.com/docker/docker/pkg/parsers/operatingsystem"
 	"github.com/docker/docker/pkg/platform"
+	"github.com/docker/docker/pkg/rpm"
 	"github.com/docker/docker/pkg/sysinfo"
 	"github.com/docker/docker/pkg/system"
 	"github.com/docker/docker/registry"
@@ -50,6 +52,14 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 	}
 
 	sysInfo := sysinfo.New(true)
+	dockerPath, err := exec.LookPath("docker")
+	if err != nil {
+		logrus.Warnf("could not look docker binary path: %v", err)
+	}
+	packageVersion, err := rpm.Version(dockerPath)
+	if err != nil {
+		logrus.Warnf("could not retrieve docker rpm version: %v", err)
+	}
 
 	var cRunning, cPaused, cStopped int32
 	daemon.containers.ApplyAll(func(c *container.Container) {
@@ -100,6 +110,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 		HTTPProxy:          getProxyEnv("http_proxy"),
 		HTTPSProxy:         getProxyEnv("https_proxy"),
 		NoProxy:            getProxyEnv("no_proxy"),
+		PkgVersion:         packageVersion,
 	}
 
 	// TODO Windows. Refactor this more once sysinfo is refactored into
@@ -125,6 +136,7 @@ func (daemon *Daemon) SystemInfo() (*types.Info, error) {
 
 // SystemVersion returns version information about the daemon.
 func (daemon *Daemon) SystemVersion() types.Version {
+	pkgVersion, _ := rpm.Version("/usr/bin/docker")
 	v := types.Version{
 		Version:      dockerversion.Version,
 		GitCommit:    dockerversion.GitCommit,
@@ -133,6 +145,7 @@ func (daemon *Daemon) SystemVersion() types.Version {
 		Arch:         runtime.GOARCH,
 		BuildTime:    dockerversion.BuildTime,
 		Experimental: utils.ExperimentalBuild(),
+		PkgVersion:   pkgVersion,
 	}
 
 	if kernelVersion, err := kernel.GetKernelVersion(); err == nil {
